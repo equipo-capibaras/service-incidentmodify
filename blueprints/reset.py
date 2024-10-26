a936ff28a4cb6ea250ce56b861bf9c0f5c6c7ebe
Tim Pambor <t.pambor@uniandes.edu.co>
2024-10-26 23:14:24 +0200

    Add database reset endpoint

---
+++ blueprints/reset.py
@@ -0,0 +1,32 @@
+from dependency_injector.wiring import Provide, inject
+from flask import Blueprint, Response, request
+from flask.views import MethodView
+
+import demo
+from containers import Container
+from repositories import IncidentRepository
+
+from .util import class_route, json_response
+
+blp = Blueprint('Reset database', __name__)
+
+
+@class_route(blp, '/api/v1/reset/incidentmodify')
+class ResetDB(MethodView):
+    init_every_request = False
+
+    @inject
+    def post(
+        self,
+        incident_repo: IncidentRepository = Provide[Container.incident_repo],
+    ) -> Response:
+        incident_repo.delete_all()
+
+        if request.args.get('demo', 'false') == 'true':
+            for incident in demo.incidents:
+                incident_repo.create(incident)
+
+                for entry in demo.history[incident.id]:
+                    incident_repo.append_history_entry(entry)
+
+        return json_response({'status': 'Ok'}, 200)
---
+++ demo/__init__.py
@@ -0,0 +1,3 @@
+from .data import history, incidents
+
+__all__ = ['history', 'incidents']
