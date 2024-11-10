3d34b48c1fa7dde7b038d7e7c80140dbcc33e128
Tim Pambor <t.pambor@uniandes.edu.co>
2024-11-10 14:56:55 +0100

    Configure permissions for pubsub

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -1,3 +1,18 @@
 resource "google_pubsub_topic" "incident_update" {
   name = "incident-update"
 }
+
+data "google_iam_policy" "pubusb" {
+  binding {
+    role = "roles/pubsub.publisher"
+    members = [
+      google_service_account.service.member,
+    ]
+  }
+}
+
+resource "google_pubsub_topic_iam_policy" "incident_update" {
+  project = google_pubsub_topic.incident_update.project
+  topic = google_pubsub_topic.incident_update.name
+  policy_data = data.google_iam_policy.pubusb.policy_data
+}
