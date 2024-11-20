ab592aa5e581daba2ffdb789968f928dfbab1d98
Laura Molina <ld.molina11@uniandes.edu.co>
2024-11-19 21:04:04 -0500

    :bug: cast types

--- tests/util.py
+++ tests/util.py
@@ -8,6 +8,7 @@ from models import Action, Channel, HistoryEntry, Incident, Risk
 
 def create_random_incident(
     faker: Faker,
+    *,
     overrides: dict[str, str | None] | None = None,
 ) -> Incident:
     overrides = overrides or {}
@@ -16,11 +17,11 @@ def create_random_incident(
         id=cast(str, faker.uuid4()),
         client_id=overrides.get('client_id') or cast(str, faker.uuid4()),
         name=faker.sentence(3),
-        channel=faker.random_element(list(Channel)),
+        channel=cast(Channel, faker.random_element(list(Channel))),
         reported_by=overrides.get('reported_by') or cast(str, faker.uuid4()),
         created_by=overrides.get('created_by') or cast(str, faker.uuid4()),
         assigned_to=overrides.get('assigned_to') or cast(str, faker.uuid4()),
-        risk=Risk(risk_value) if risk_value is not None else faker.random_element(list(Risk)),
+        risk=Risk(risk_value) if risk_value is not None else cast(Risk, faker.random_element(list(Risk))),
     )
 
 
@@ -36,7 +37,7 @@ def create_random_history_entry(
         incident_id=incident_id or cast(str, faker.uuid4()),
         client_id=client_id or cast(str, faker.uuid4()),
         date=faker.past_datetime(tzinfo=UTC),
-        action=action or faker.random_element(list(Action)),
+        action=action or cast(Action, faker.random_element(list(Action))),
         description=faker.text(),
         seq=seq,
     )
